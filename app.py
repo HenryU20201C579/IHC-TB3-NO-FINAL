2eacd26800218aa726fe314c4c94aa1c40e18dea
HenryU20201C579 <u20201579@upc.edu.pe>
2023-06-01 01:54:41 -0500

    peque

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-print("felicidad")
+from flask import Flask, render_template, request, redirect
+from flask_mysqldb import MySQL
+from flaskext.mysql import MySQL
+
+app = Flask(__name__)
+mysql = MySQL()
+
+@app.route('/')
+def inicio():
+    return render_template('sitio/index.html')
+
+@app.route('/contactanos')
+def contactanos():
+    return render_template('sitio/contactanos.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
