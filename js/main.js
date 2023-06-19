88f7c05b56924cfb46fc173bea89411e5990016c
HenryU20201C579 <u20201579@upc.edu.pe>
2023-06-19 04:02:50 -0500

    TB4

---
+++ js/main.js
@@ -0,0 +1,20 @@
+function openNav(){
+  document.getElementById("mobile-menu").style.width = "100%";
+}
+
+function closeNav(){
+  document.getElementById("mobile-menu").style.width = "0%";
+}
+
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true
+  });
+
+  // element argument can be a selector string for an individual element
+var flkty = new Flickity( '.main-carousel', {
+    // options
+    });
+    
