2d77be0d4062b09009c1c37494c43d6eda4c28a2
HenryU20201C579 <u20201579@upc.edu.pe>
2023-06-21 22:31:37 -0500

    tb4-final

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 mysql = MySQL()
 
-app.config['MYSQL_DATABASE_HOST'] = '3.82.36.72'
+app.config['MYSQL_DATABASE_HOST'] = '54.144.71.125'
 app.config['MYSQL_DATABASE_USER'] = 'support'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'sistema.db'
 app.config['MYSQL_DATABASE_DB'] = 'tb3'
@@ -92,6 +92,54 @@ def resultados():
     resultados = cur.fetchall()
     return render_template('resultados.html', resultados = resultados)
 
+@app.route('/resultados_fis', methods=["post"])
+def resultados_fis():
+    nombre = request.form['name']
+    distrito = request.form['distrito']
+    dia = request.form['dia']
+    opcion_horario = request.form['horario']
+
+    # Crear una lista para almacenar las cláusulas de búsqueda
+    condiciones = []
+    parametros = []
+
+    # Construir la consulta SQL y los parámetros
+    consulta = "SELECT * FROM fisioterapeuta"
+
+    if nombre:
+        condiciones.append("nombre = %s")
+        parametros.append(nombre)
+
+    if distrito:
+        condiciones.append("distrito = %s")
+        parametros.append(distrito)
+
+    if dia:
+        condiciones.append("dias = %s")
+        parametros.append(dia)
+
+    if opcion_horario == "Mañana (7:00 - 12:00)":
+        condiciones.append("hora_inicio BETWEEN '07:00:00' AND '12:00:00'")
+    elif opcion_horario == "Tarde (12:00 - 18:00)":
+        condiciones.append("hora_inicio BETWEEN '12:00:00' AND '18:00:00'")
+    elif opcion_horario == "Noche (18:00 - 9:00)":
+        condiciones.append("hora_inicio BETWEEN '18:00:00' AND '21:00:00'")
+
+    # Combinar las cláusulas de búsqueda si hay alguna
+    if condiciones:
+        consulta += " WHERE " + " AND ".join(condiciones)
+    print(consulta, parametros)
+    
+    consulta += ";"
+
+
+    conexion = mysql.connect()
+    cur = conexion.cursor()
+    cur.execute(consulta, parametros)
+    resultados = cur.fetchall()
+    return render_template('resultados.html', resultados = resultados)
+
+
 @app.route('/find_vol')
 def find_vol():
     return render_template('find_vol.html')
@@ -127,15 +175,10 @@ def saveDataEjercicios():
     cursor.execute("insert into ejercicios (ejercicio, repeticiones, fecha) values ( %s, %s, %s);", data)
     conexion.commit()
 
-
     return redirect('/ejercicios')
 
 @app.route('/saveDataVoluntario', methods=['POST'])
 def saveDataVoluntario():
-    #lunes, martes, miercoles, jueves, viernes, sabado, domingo = "hola"
-    # if request.method == 'POST':
-    #     print(request.form.getlist('jueves'))
-    
     nombre = request.form['name']
     distrito = request.form['distrito']
 
@@ -171,6 +214,38 @@ def saveDataVoluntario():
                     conexion.commit()
 
     return redirect('/registro_vol')
+
+
+@app.route('/saveDataFisioterapeuta', methods=['POST'])
+def saveDataFisioterapeuta():
+    nombre = request.form['name']
+    distrito = request.form['distrito']
+    especializacion = request.form['Especialización']
+    certificaciones = request.form['Certificationes']
+
+    dias = []
+    dias.append(request.form.getlist('lunes'))
+    dias.append(request.form.getlist('martes'))
+    dias.append(request.form.getlist('miercoles'))
+    dias.append(request.form.getlist('jueves'))
+    dias.append(request.form.getlist('viernes'))
+    dias.append(request.form.getlist('sabado'))
+    dias.append(request.form.getlist('domingo'))
+    
+    hora_inicio = request.form['hora_inicio']
+    hora_final = request.form['hora_final']
+
+
+    conexion = mysql.connect()
+    cursor = conexion.cursor()
+    
+    for var in dias: 
+        if (var != [] and hora_inicio != "" and hora_final != "" and (hora_inicio < hora_final) and nombre != "" and distrito != "" and especializacion != "" and certificaciones != ""):
+            data = (nombre, distrito, var, hora_inicio, hora_final, especializacion, certificaciones)
+            cursor.execute("insert into fisioterapeuta (nombre, distrito, dias, hora_inicio, hora_final, especializacion, certificaciones) values (%s, %s, %s, %s, %s, %s, %s);", data)
+            conexion.commit()
+
+    return redirect('/registro_fis')
 
 
 if __name__ == "__main__":
